404b7e8aa6bdc5e3de6a65b394420d64524c2ec6
Heunsig <heun3344@gmail.com>
2019-09-03 23:52:30 +0900

    api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Models\Admin\Content;
+use App\Models\Admin\Portfolio;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,79 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+// 
+
+function hasProperty ($object, $property) {
+    foreach ($object as $key => $value) {
+        if ($key == $property) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function filterProperty ($elements, $value) {
+    $result = [];
+    foreach($elements as $element) {
+        if (hasProperty($value, $element)) {
+            $result[$element] = $value[$element];
+        }
+    }
+
+    return $result;
+}
+
+function makeResult ($value, $onlyQuery) {
+    $result = [];
+
+    if ($value) {
+        $value = $value->toArray();
+
+        if ($onlyQuery) {
+            $elementsToFilter = explode(',', $onlyQuery);
+
+            $result = filterProperty($elementsToFilter, $value);
+        } else {
+            $result = $value;
+        }
+    }
+
+
+    return $result;
+} 
+
+Route::get('contents', function (Request $request) {
+    $contents = Content::all();
+    $result = [];
+
+    foreach ($contents as $content) {
+        $result[] = makeResult($content, $request->query('only'));
+    }
+
+    return response()->json($result);
+});
+
+Route::get('contents/{id}', function (Request $request, $id) {
+    $content = Content::where('id', $id)->first();
+    return response()->json(makeResult($content, $request->query('only')));
+});
+
+Route::get('portfolios', function (Request $request) {
+    $portfolios = Portfolio::with('links', 'files')->get();
+    $result = [];
+
+    foreach ($portfolios as $portfolio) {
+        $result[] = makeResult($portfolio, $request->query('only'));
+    }
+
+    return response()->json($result);
+});
+
+Route::get('portfolios/{id}', function (Request $request, $id) {
+    $portfolio = Portfolio::where('id', $id)->with('links', 'files')->first();
+    return response()->json(makeResult($portfolio, $request->query('only')));
 });
