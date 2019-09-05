b2d0156abcedcaa481526f07abeaeeebb050d6be
Heunsig <heun3344@gmail.com>
2019-09-05 21:43:22 +0900

    comment out

--- app/Http/Middleware/CheckReferrer.php
+++ app/Http/Middleware/CheckReferrer.php
@@ -19,9 +19,9 @@ class CheckReferrer
     {
         $key = $request->query('key');
         $apikey = Apikey::where('key', $key)->first();
-        // $fullReferrer = $request->server('HTTP_REFERER');
-        // $parsed_url = parse_url($fullReferrer);
-        // $domain = $parsed_url['scheme'] . '://' . $parsed_url['host'] . ($parsed_url['port'] ? ':'.$parsed_url['port'] : '');
+        $fullReferrer = $request->server('HTTP_REFERER');
+        $parsed_url = parse_url($fullReferrer);
+        $domain = $parsed_url['scheme'] . '://' . $parsed_url['host'] . ($parsed_url['port'] ? ':'.$parsed_url['port'] : '');
 
         if(!$apikey) {
             return response()->json(['error' => 'Invalid api key']);
