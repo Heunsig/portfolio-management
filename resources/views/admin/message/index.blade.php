233a8ae24cb2e4d94bc64a0283e98af5fb222e1f
Heunsig <heun3344@gmail.com>
2019-09-15 22:46:21 +0900

    added layout.blade

--- resources/views/admin/message/index.blade.php
+++ resources/views/admin/message/index.blade.php
@@ -1,92 +1,48 @@
-@extends('admin.main')
+@extends('admin.message.layout')
 
 @section('title','- Message')
 
-@section('content')
-<div class="ui grid">
-	<div class="sixteen wide column">
-		<div class="ui grid catcha c-header-main">
-			<div class="twelve wide column">
-				<h1 class="ui header catcha c-header-title">Message</h1>
-				<div class="ui breadcrumb">
-				  <a class="section">Home</a>
-				  <div class="divider"> / </div>
-				  <div class="section active">Message</div>
-				</div>
-			</div>
-			<div class="four wide column right aligned">
+@section('content.breadcrumb')
+	<a class="section">Home</a>
+  <div class="divider"> / </div>
+  <div class="section active">Message</div>
+@endsection
 
-			</div>
-		</div>
-	</div>
+@section('content.content')
+
 	<h2>Message List</h2>
-	<div class="sixteen wide column">
-		<div class="ui segment">
-			<table class="ui very basic celled table">
-				<thead>
-					<tr>
-						<th width="50">#</th>
-						<th width="250">Customer name</th>
-						<th>Email</th>
-						<th width="200">Sent at</th>
-						<th width="100"></th>
-					</tr>
-				</thead>	
-				<tbody>
-					@if(count($messages))
-						@foreach($messages as $message)
-						<tr>
-							<td>{{ $message->id }}</td>
-							<td>{{ $message->name }}</td>
-							<td>{{ $message->email }}</td>
-							<td>{{ $message->created_at->diffForHumans() }}</td>
-							<td class="center aligned">
-								<a href="{{ route('admin.messages.show', $message->id) }}" class="ui tiny positive button">View</a>
-							</td>
-						</tr>
-						@endforeach
-					@else
-						<tr>
-							<td colspan="4" class="center aligned">No message</td>
-						</tr>
-					@endif
-				</tbody>
-			</table>
-		</div>
-	</div>
-</div>
-
-{{-- <div class="row">
-	<div class="col-md-12">
-		<ol class="breadcrumb">
-			<li class="active">Message</li>
-		</ol>
-	</div>
-	<div class="col-md-12">
-		<h2>Message</h2>
-		<table class="table">
+	<div class="ui segment">
+		<table class="ui very basic celled table">
 			<thead>
 				<tr>
-					<th>#</th>
-					<th>Name</th>
+					<th width="50">#</th>
+					<th width="250">Customer name</th>
 					<th>Email</th>
-					<th></th>
+					<th width="200">Sent at</th>
+					<th width="100"></th>
 				</tr>
-			</thead>
+			</thead>	
 			<tbody>
-				@foreach($messages as $message)
-				<tr>
-					<td>{{ $message->id }}</td>
-					<td>{{ $message->name }}</td>
-					<td>{{ $message->email }}</td>
-					<td>
-						<a href="{{ route('admin.messages.show', $message->id) }}" class="btn btn-default">View</a>
-					</td>
-				</tr>
-				@endforeach
+				@if(count($messages))
+					@foreach($messages as $message)
+					<tr>
+						<td>{{ $message->id }}</td>
+						<td>{{ $message->name }}</td>
+						<td>{{ $message->email }}</td>
+						<td>{{ $message->created_at->diffForHumans() }}</td>
+						<td class="center aligned">
+							<a href="{{ route('admin.messages.show', $message->id) }}" class="ui tiny positive button">View</a>
+						</td>
+					</tr>
+					@endforeach
+				@else
+					<tr>
+						<td colspan="4" class="center aligned">No message</td>
+					</tr>
+				@endif
 			</tbody>
 		</table>
 	</div>
-</div> --}}
+
 @endsection
 
--- resources/views/admin/message/show.blade.php
+++ resources/views/admin/message/show.blade.php
@@ -1,111 +1,52 @@
-@extends('admin.main')
+@extends('admin.message.layout')
 
 @section('title', '- Message '. $message->name)
 
-@section('content')
-<div class="ui grid">
-	<div class="sixteen wide column">
-		<div class="ui grid catcha c-header-main">
-			<div class="twelve wide column">
-				<h1 class="ui header catcha c-header-title">Message</h1>
-				<div class="ui breadcrumb">
-				  <a class="section">Home</a>
-				  <div class="divider"> / </div>
-				  <a class="section" href="{{ route('admin.messages.index') }}">Message</a>
-				  <div class="divider"> / </div>
-				  <div class="section  active">View: {{ $message->id }}</div>
-				</div>
-			</div>
-			<div class="four wide column right aligned">
-				<a href="{{ route('admin.messages.index') }}" class="ui primary button">Back to list</a>
-				{{ Form::open(['route'=>['admin.message.destroy', $message->id], 'method'=>'DELETE', 'class'=>'catcha c-alignment-inline', 'id'=>'formDeleteMessage']) }}
-					<button type="button" class="ui red button" id="btnDeleteMessage">Delete</button>
-				{{ Form::close() }}
-			</div>
-		</div>
-	</div>
+@section('content.breadcrumb')
+	<a class="section">Home</a>
+  <div class="divider"> / </div>
+  <a class="section" href="{{ route('admin.messages.index') }}">Message</a>
+  <div class="divider"> / </div>
+  <div class="section  active">View: {{ $message->id }}</div>
+@endsection
+
+@section('content.topButtons')
+	<a href="{{ route('admin.messages.index') }}" class="ui primary button">Back to list</a>
+	{{ Form::open(['route'=>['admin.message.destroy', $message->id], 'method'=>'DELETE', 'class'=>'catcha c-alignment-inline', 'id'=>'formDeleteMessage']) }}
+		<button type="button" class="ui red button" id="btnDeleteMessage">Delete</button>
+	{{ Form::close() }}
+@endsection
+
+@section('content.content')
+
 	<h2>Read message #{{ $message->id }} from {{ $message->name }}</h2>
-	<div class="sixteen wide column">
-		<div class="ui padded segment">
-			<div class="ui grid">
-				<div class="row">
-					<div class="four wide column">
-						<div class="ui tiny header">Customer name</div>
-						<p>{{ $message->name }}</p>
-					</div>
-					<div class="six wide column">
-						<div class="ui tiny header">Customer email</div>
-						<p>{{ $message->email }}</p>
-					</div>
-					<div class="four wide column">
-						<div class="ui tiny header">Sent at</div>
-						<p>
-							{{ $message->created_at->diffForHumans() }} ({{ $message->created_at->format('Y-m-d h:m A') }})
-						</p>
-					</div>
+	<div class="ui padded segment">
+		<div class="ui grid">
+			<div class="row">
+				<div class="four wide column">
+					<div class="ui tiny header">Customer name</div>
+					<p>{{ $message->name }}</p>
 				</div>
-				<div class="ui divider"></div>
-				<div class="row">
-					<div class="column">
-						<div class="ui tiny header">Content</div>
-						<p>{{ $message->message }}</p>
-					</div>
+				<div class="six wide column">
+					<div class="ui tiny header">Customer email</div>
+					<p>{{ $message->email }}</p>
+				</div>
+				<div class="four wide column">
+					<div class="ui tiny header">Sent at</div>
+					<p>
+						{{ $message->created_at->diffForHumans() }} ({{ $message->created_at->format('Y-m-d h:m A') }})
+					</p>
+				</div>
+			</div>
+			<div class="ui divider"></div>
+			<div class="row">
+				<div class="column">
+					<div class="ui tiny header">Content</div>
+					<p>{{ $message->message }}</p>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
-
-
-
-{{-- <div class="row">
-	<div class="col-md-12">
-		<ol class="breadcrumb">
-			<li><a href="{{ route('admin.messages.index') }}">Message</a></li>
-			<li class="active">View</li>
-		</ol>
-	</div>
-	<div class="col-md-12">
-		<div class="page-header">
-			<h1>{{ $message->name }}</h1>
-		</div>
-		<div class="row">
-			<div class="col-md-8">
-				<div class="row">
-					<div class="col-md-12">{{ $message->message }}</div>
-				</div>
-			</div>	
-			<div class="col-md-4">
-				<div class="list-group">
-					<div href="#" class="list-group-item">
-						<h4 class="list-group-item-heading">Email</h4>
-						<p class="list-group-item-text">{{ $message->email }}</p>
-					</div>
-					<div href="#" class="list-group-item">
-						<h4 class="list-group-item-heading">Created At</h4>
-						<p class="list-group-item-text">{{ $message->created_at }}</p>
-					</div>
-					<div href="#" class="list-group-item">
-						<h4 class="list-group-item-heading">Updated At</h4>
-						<p class="list-group-item-text">{{ $message->updated_at }}</p>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-12">
-						{{ Form::open(['route'=>['admin.messages.destroy', $message->id], 'method'=>'DELETE']) }}
-						<button class="btn btn-danger btn-block">Delete</button>
-						{{ Form::close() }}
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-12">
-						<a href="{{ route('admin.messages.index') }}" class="btn btn-default btn-block space-margin-top"><< See All of messages</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div> --}}
 
 @component('admin.components.modals.checkingModal')
 	@slot('id')
