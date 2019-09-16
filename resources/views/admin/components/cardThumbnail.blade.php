99677b86ba519b99966653b91e379feb725a4adb
Heunsig <heun3344@gmail.com>
2019-09-16 19:19:49 +0900

    added default height css

--- resources/views/admin/components/cardThumbnail.blade.php
+++ resources/views/admin/components/cardThumbnail.blade.php
@@ -1,9 +1,13 @@
 @php
   $class = 'image catcha c-image-card-thumbnail';
-  $style = 'background:url(' . $picture . ');background-size:cover;background-position:center;';
+  $style = 'background:url(' . $picture . ');';
+  $styleForEmpty = '';
+
+  isset($height) && $height ? $style .= 'height:' .  $height .';' : 'height:150px;';
+  isset($height) && $height ? $styleForEmpty .= 'height:' . $height .';' : 'height:150px;';
 @endphp
 
-@if(isset($picture))
+@if(isset($picture) && $picture)
   <{{ isset($tag) && $tag ? $tag : 'div' }} 
     @if(isset($attrs) && $attrs)
       @foreach($attrs as $key => $value)
@@ -13,7 +17,7 @@
           @endphp
         @elseif($key === 'style')
           @php
-            $style .= ' ' . $value 
+            $style .= ' ' . $value
           @endphp
         @else
           {{ $key }}="{{$value}}"
@@ -23,10 +27,16 @@
     
     class='{{ $class }}'
     style='{{ $style }}'
+
+    {{-- height="{{ isset($height) && $height ? $height : '200px' }}" --}}
   >
   </{{ isset($tag) && $tag ? $tag : 'div' }}>
 @else
-  <{{ isset($tag) && $tag ? $tag : 'div' }} class="catcha c-image-empty-card">
+  <{{ isset($tag) && $tag ? $tag : 'div' }} 
+    class="catcha c-image-empty-card"
+    style='{{ $styleForEmpty }}'
+    {{-- height="{{ isset($height) && $height ? $height : '200px' }}" --}}
+  >
     <i class="ui icon image outline"></i>
   </{{ isset($tag) && $tag ? $tag : 'div' }}>
 @endif
