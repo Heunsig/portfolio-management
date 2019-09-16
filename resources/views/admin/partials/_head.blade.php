d7132c0d300c05ce2d9e15a66cd27edfaec6cd45
Heunsig <heun3344@gmail.com>
2019-09-16 21:56:32 +0900

    removed commets

--- resources/views/admin/partials/_head.blade.php
+++ resources/views/admin/partials/_head.blade.php
@@ -4,11 +4,6 @@
 <meta name="description" content="">
 <meta name="author" content="">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-
-{{-- {{ Html::style('assets/common_lib/jquery/jquery-ui.min.css') }}
-{{ Html::style('assets/common_lib/bootstrap/css/bootstrap.min.css') }}
-{{ Html::style('assets/common_lib/semantic-icon/icon.min.css') }}
-{{ Html::style('assets/admin/css/bootstrap.extend.css') }} --}}
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" />
 {{ Html::style('assets/admin/css/style.css') }}
 @stack('stylesheets')
--- resources/views/admin/partials/_scripts.blade.php
+++ resources/views/admin/partials/_scripts.blade.php
@@ -1,7 +1,3 @@
-
-{{-- {{ Html::script('assets/common_lib/jquery/jquery-2.1.4.min.js') }}
-{{ Html::script('assets/common_lib/jquery/jquery-ui.min.js') }}
-{{ Html::script('assets/common_lib/bootstrap/js/bootstrap.min.js') }} --}}
 <script
   src="https://code.jquery.com/jquery-3.1.1.min.js"
   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
@@ -55,36 +51,3 @@
 </script>
 
 @stack('scripts')
-
-{{-- <script>
-	$(document).ready(function(){
-		activeCurrentNav();
-
-		/*
-		 *	ajax set headers token (Laravel need this token)
-		 */
-		if(typeof documentReady === 'function'){
-			$.ajaxSetup({
-			        headers: {
-			            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-			        }
-			});
-			
-			documentReady();
-		}
-	});
-
-	function activeCurrentNav(){
-		// Common Script
-		var breadcrumb_content = $('.breadcrumb').children().eq(0);
-		var current_page = breadcrumb_content.text().toLowerCase();
-
-		$('#main_nav').children("li[role='presentation']").each(function(i,e){
-			var a = $(e).children('a').text().toLowerCase();
-				if($(e).children('a').text().toLowerCase() === current_page){
-					$(e).addClass('active');
-					return false;
-				}
-		});
-	}
-</script> --}}
