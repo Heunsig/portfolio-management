7eacc0fc8e4c54a1c74201bbe90329f6b796597d
Heunsig <heun3344@gmail.com>
2019-09-16 20:46:12 +0900

    removed the leagcy code

--- app/Models/Admin/Category.php
+++ app/Models/Admin/Category.php
@@ -9,8 +9,4 @@ class Category extends Model
     public function portfolios(){
   		return $this->belongsToMany('App\Models\Admin\Portfolio', 'rel_portfolio_category', 'category_id', 'portfolio_id');
   	}
-
-	public function templates(){
-		return $this->belongsToMany('App\Models\Admin\Template', 'rel_template_category', 'category_id', 'template_id');
-	}
 }
